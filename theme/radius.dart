645bc7ddfbd1d0fa18c00c3172ce8908f8d9d177
tolgaeke18-jpg <tolgaeke18@gmail.com>
2026-08-05 10:47:46 +0300

    Create radius.dart

---
+++ theme/radius.dart
@@ -0,0 +1,11 @@
+class AtlasRadius {
+  AtlasRadius._();
+
+  static const double small = 8;
+
+  static const double medium = 12;
+
+  static const double large = 20;
+
+  static const double xlarge = 32;
+}
