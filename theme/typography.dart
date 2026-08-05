a0ccea8ede16869ccc3cba85ccc6d6c692397e21
tolgaeke18-jpg <tolgaeke18@gmail.com>
2026-08-05 10:47:58 +0300

    Create typography.dart

---
+++ theme/typography.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AtlasTypography {
+  AtlasTypography._();
+
+  static const TextStyle headline =
+      TextStyle(fontSize: 32, fontWeight: FontWeight.bold);
+
+  static const TextStyle title =
+      TextStyle(fontSize: 22, fontWeight: FontWeight.w700);
+
+  static const TextStyle body =
+      TextStyle(fontSize: 16);
+
+  static const TextStyle caption =
+      TextStyle(fontSize: 13);
+}
