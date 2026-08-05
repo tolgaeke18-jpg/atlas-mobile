bc051dcfb6a54f182cec6f693ed422085625f6a3
tolgaeke18-jpg <tolgaeke18@gmail.com>
2026-08-05 13:32:46 +0300

    Update app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'theme.dart';
+import 'app_theme.dart';
+import 'router.dart';
 
 class AtlasApp extends StatelessWidget {
   const AtlasApp({super.key});
@@ -9,64 +10,16 @@ class AtlasApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Atlas',
+
       debugShowCheckedModeBanner: false,
 
-      theme: AtlasTheme.lightTheme,
-      darkTheme: AtlasTheme.darkTheme,
+      theme: AtlasTheme.light,
+
+      darkTheme: AtlasTheme.dark,
+
       themeMode: ThemeMode.system,
 
-      home: const SplashPage(),
-    );
-  }
-}
-
-class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            Icon(
-              Icons.electric_car,
-              color: Colors.white,
-              size: 80,
-            ),
-
-            SizedBox(height: 24),
-
-            Text(
-              "ATLAS",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 34,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 6,
-              ),
-            ),
-
-            SizedBox(height: 12),
-
-            Text(
-              "Drive Smarter.\nCharge Better.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white70,
-                fontSize: 16,
-              ),
-            ),
-
-            SizedBox(height: 40),
-
-            CircularProgressIndicator()
-          ],
-        ),
-      ),
+      onGenerateRoute: AtlasRouter.generate,
     );
   }
 }
