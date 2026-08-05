78a50568ad3789b7a9cb45d41e818aacf71ceccd
tolgaeke18-jpg <tolgaeke18@gmail.com>
2026-08-05 10:47:10 +0300

    Create colors.dart

---
+++ theme/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AtlasColors {
+  AtlasColors._();
+
+  static const Color primary = Color(0xFF2563EB);
+
+  static const Color secondary = Color(0xFF14B8A6);
+
+  static const Color success = Color(0xFF22C55E);
+
+  static const Color warning = Color(0xFFF59E0B);
+
+  static const Color danger = Color(0xFFEF4444);
+
+  static const Color backgroundDark = Color(0xFF09090B);
+
+  static const Color backgroundLight = Color(0xFFF8FAFC);
+
+  static const Color cardDark = Color(0xFF18181B);
+
+  static const Color cardLight = Colors.white;
+
+  static const Color textDark = Colors.white;
+
+  static const Color textLight = Color(0xFF111827);
+
+  static const Color grey = Color(0xFF9CA3AF);
+}
