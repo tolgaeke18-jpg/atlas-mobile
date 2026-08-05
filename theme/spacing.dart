97421aede0a2ba307a86dcca9c8edfb2c1df339b
tolgaeke18-jpg <tolgaeke18@gmail.com>
2026-08-05 10:47:30 +0300

    Create spacing.dart

---
+++ theme/spacing.dart
@@ -0,0 +1,15 @@
+class AtlasSpacing {
+  AtlasSpacing._();
+
+  static const double xs = 4;
+
+  static const double sm = 8;
+
+  static const double md = 16;
+
+  static const double lg = 24;
+
+  static const double xl = 32;
+
+  static const double xxl = 48;
+}
