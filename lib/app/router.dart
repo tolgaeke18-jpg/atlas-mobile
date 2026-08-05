657b0661195ae5245fbdae81029ce8af59e153aa
tolgaeke18-jpg <tolgaeke18@gmail.com>
2026-08-05 13:31:53 +0300

    Update router.dart

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import '../features/splash/presentation/pages/splash_page.dart';
 
 class AtlasRouter {
-  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  AtlasRouter._();
+
+  static Route<dynamic> generate(RouteSettings settings) {
     switch (settings.name) {
       default:
         return MaterialPageRoute(
